acab0ff952ac0e95cd3ae76131b7ab8745a9cad9
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-01 23:10:56 -0300

    Added Home & Heared component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello 🤪</h1>
+        {/* BEM */}
+        <Header />
+        <Home />
     </div>
   );
 }
