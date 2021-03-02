156f9f84ffcd5b21361ea29c9087061bbbfaec7b
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-01 23:12:09 -0300

    Created & styled Header component

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import cartLogo from './../images/amazon-cart.svg';
+
+function Header() {
+    return (
+        <div className="header">
+            <img
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
+                alt=""
+            />
+
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello guest</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+                <div className="header__option header__optionBasketContainer">
+                    <img
+                        className="header__optionBasketImg"
+                        src={cartLogo}
+                        alt=""
+                    />
+                    <span className="header__optionLineTwo header__basketCount">
+                        0
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header
