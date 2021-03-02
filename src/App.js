b3bd3ec516fbec10f1b2af9dd666f708b3019737
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-01 23:45:00 -0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-
+import Checkout from './components/Checkout';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-        {/* BEM */}
-        <Header />
-        <Home />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route path="/checkout">
+                        <Checkout />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
