e6b69e8db29335eb881a1c6970f41076e32b43bf
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-04 10:13:32 -0300

    Created & styled Payment component

---
+++ src/components/Payment.js
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react';
+import { useStateValue } from './../StateProvider';
+import { Link, useHistory } from 'react-router-dom';
+import CheckoutProduct from './CheckoutProduct';
+import './Payment.css';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { getBasketTotal } from '../reducer';
+import CurrencyFormat from 'react-currency-format';
+import axios from './../axios';
+import { db } from './../firabase';
+
+function Payment() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
+    const stripe = useStripe();
+    const elements = useElements();
+    const history = useHistory();
+
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState('');
+    const [disabled, setDisabled] = useState(true);
+    const [error, setError] = useState(null);
+    const [clientSecret, setClientSecret] = useState(true);
+
+    useEffect(() => {
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: 'post',
+                // stripe expects the total in currency subnuits
+                url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
+            });
+            setClientSecret(response.data.clientSecret);
+        };
+
+        getClientSecret();
+    }, [basket]);
+
+    console.log('THE SECRET IS >>>>', clientSecret);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setProcessing(true);
+
+        const payload = await stripe
+            .confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                },
+            })
+            .then(({ paymentIntent }) => {
+                db.collection('users')
+                    .doc(user?.currentUser.uid)
+                    .collection('orders')
+                    .doc(paymentIntent.id)
+                    .set({
+                        basket: basket,
+                        amount: paymentIntent.amount,
+                        created: paymentIntent.created,
+                    });
+
+                setSucceeded(true);
+                setError(null);
+                setProcessing(false);
+
+                dispatch({
+                    type: 'EMPTY_BASKET',
+                });
+
+                history.replace('/orders');
+            });
+    };
+
+    const handleChange = (event) => {
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : '');
+    };
+
+    return (
+        <div className="payment">
+            <div className="payment__container">
+                <h1>
+                    Checkout (<Link to="/checkout">{basket?.length} items</Link>
+                    )
+                </h1>
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Delivery Address</h3>
+                    </div>
+                    <div className="payment__address">
+                        <p>{user?.currentUser.email}</p>
+                        <p>123 React Lane</p>
+                        <p>Los Angeles, CA</p>
+                    </div>
+                </div>
+
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Review itmes and delivery</h3>
+                    </div>
+                    <div className="payment__items">
+                        {basket.map((item) => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                img={item.img}
+                                price={item.price}
+                                rating={item.rating}
+                                quantity={item.quantity}
+                                hideButton
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Payment details</h3>
+                    </div>
+                    <div className="payment__details">
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
+                            <div className="payment__priceContaine">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>
+                                            Order Total:
+                                            <strong> {value}</strong>
+                                        </h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={'text'}
+                                    thousandSeparator={true}
+                                    prefix={'$'}
+                                />
+                            </div>
+                            <button
+                                className="payment__detailsButton"
+                                disabled={
+                                    processing || disabled || succeeded
+                                }
+                            >
+                                <span>
+                                    {processing ? (
+                                        <p>Processing</p>
+                                    ) : (
+                                        'Buy Now'
+                                    )}
+                                </span>
+                            </button>
+
+                            {/* Show Error */}
+                            {error && <div>{error}</div>}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Payment;
