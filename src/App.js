8949c61a9338abd45991192c33fdf6d87531c0f3
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-04 10:43:56 -0300

    Imported components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login';
 import Checkout from './components/Checkout';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Payment from './components/Payment';
+import Orders from './components/Orders';
+import { auth } from './firabase';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_TEST_KEY);
 
 function App() {
+    const [{}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                // A user logged in
+                dispatch({
+                    type: 'SET_USER',
+                    payload: {
+                        user: auth,
+                    },
+                });
+            } else {
+                // A user logded out
+                dispatch({
+                    type: 'SET_USER',
+                    payload: {
+                        user: null,
+                    },
+                });
+            }
+        });
+    }, []);
+
     return (
         <Router>
             <div className="App">
-                <Header />
                 <Switch>
                     <Route path="/checkout">
+                        <Header />
                         <Checkout />
                     </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/payment">
+                        <Header />
+                        <Elements stripe={promise}>
+                            <Payment />
+                        </Elements>
+                    </Route>
+                    <Route path="/orders">
+                        <Header />
+                        <Orders />
+                    </Route>
                     <Route path="/">
+                        <Header />
                         <Home />
                     </Route>
                 </Switch>
